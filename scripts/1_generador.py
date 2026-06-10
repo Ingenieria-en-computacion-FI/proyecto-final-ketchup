70e4e7384e0bc5a1bbbd28299192ff849eaac3ae
zw29n47bmb-dev <zw29n47bmb@privaterelay.appleid.com>
2026-06-10 00:01:56 -0600

    Update 1_generador.py

--- scripts/1_generador.py
+++ scripts/1_generador.py
@@ -1,23 +1,53 @@
-import csv
+import csv 
 import random
+import argparse
+import sys
 
-def generador_procesos(cantidad=25, archivo_salida='Procesos.csv'):	
-	with open(archivo_salida, mode = 'w', newline = '') as file:
-		writer = csv.writer(file)
-		writer.writerow(["PID", "Burst_Time", "Priority", "Memory_MB"])
-		
-		for pid in range(1, cantidad+1):
-			#Simulamos rafagas y cantidades realistas
-			burst = random.randint(1, 15)
-			prio = random.randint(1, 5)
-			
-			#Asignamos bloques de memoria en potencias de 2 (tipico en SO) o tamanos arbitrarios
-			mem_options = [32, 64, 128, 256, 512]
-			mem = random.choice(mem_options)
-			
-			writer.writerow([pid, burst, prio, mem])
+def generar_procesos(num_procesos, max_memoria, archivo_salida):
+	procesos = []
+	
+	for pid in range(1, num_procesos + 1):
+		#Inyectar "Edge cases "
+		# 15 % de probabilidad de generar un proceso muy pesado (Memory hog)
+		# Esto forzara al simulador a lidiar con la fragmentacion y la coalescencia
+		if random.random() < 0.15:
+			burst_time = random.randint(20, 50)
+			#Solicita entre el 40% y el 80% de la memoria maxima
+			memory_required = random.randint(int(max_memoria * 0.4), int(max_memoria * 0.8))
+			priority = random.randint(1, 2)
 
-	print(f"[{archivo_salida}] Generados {cantidad} procesos correctamente.")
+		else:
+			#Procesos ligeros y rapidos que llenaran huecos pequenos
+			burst_time = random.randint(1, 10)
+			memory_required = random.randint(10, int(max_memoria * 0.15))
+			priority = random.randint(3, 5)
 
-if __name__ == "__main__":
-	generador_procesos(25)
+		#El documento expresa que un proceso requiere:
+		# pid, burst_time, priority y memory_required
+		# (remaining time) y state se inicializan internamente en C
+		procesos.append([pid, burst_time, priority, memory_required])
+
+	
+	try:
+		with open(archivo_salida, mode = 'w', newline = '') as file:
+			writer = csv.writer(file)
+			writer.writerows(procesos)
+		print(f"[+] Exito: {num_procesos} procesos generados y guardados en '{archivo_salida}'.")
+
+	except IOError as e:
+		print(f"[-] Error al escribir el archivo {e}", file=sys.stderr)
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Generador de cargas de trabajo para simulador de OS')
+
+	#Definicion de argumentos por linea de comandos
+	parser.add_argument('-n', '--num', type=int, default=100, 
+			   help='Numero de procesos a generar (default: 100)')
+	parser.add_argument('-m', '--mem', type=int, default=1024, 
+			   help='Memoria maxima del sistema simulado para calcular proporciones (default: 1024)')
+	parser.add_argument('-o', '--out', type=str, default='procesos.csv', 
+			help='Archivo de salida (default: procesos.csv)')
+
+	args = parser.parse_args()
+	generar_procesos(args.num, args.mem, args.out)
+
